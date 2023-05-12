1f074c73cf309437f5a71f2e8db568b40e8c6973
Boris Pavlov <borzpavlov@gmail.com>
2023-05-12 20:08:29 +0500

    Add project files.

---
+++ DeepParameters/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace DeepParameters {
+    partial class MainFrom {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrom));
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.WorkFileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitApp = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpAllSteps = new System.Windows.Forms.ToolStripMenuItem();
+            this.allTabs = new System.Windows.Forms.TabControl();
+            this.tab1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.selectAccident = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.acceptFaultButton = new System.Windows.Forms.Button();
+            this.menuStrip.SuspendLayout();
+            this.allTabs.SuspendLayout();
+            this.tab1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.WorkFileMenuItem,
+            this.ExitApp,
+            this.helpAllSteps});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip.ShowItemToolTips = true;
+            this.menuStrip.Size = new System.Drawing.Size(836, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // WorkFileMenuItem
+            // 
+            this.WorkFileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenFile});
+            this.WorkFileMenuItem.Name = "WorkFileMenuItem";
+            this.WorkFileMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.WorkFileMenuItem.Text = "Файл";
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(121, 22);
+            this.OpenFile.Text = "Открыть";
+            // 
+            // ExitApp
+            // 
+            this.ExitApp.Name = "ExitApp";
+            this.ExitApp.Size = new System.Drawing.Size(54, 20);
+            this.ExitApp.Text = "Выход";
+            // 
+            // helpAllSteps
+            // 
+            this.helpAllSteps.Name = "helpAllSteps";
+            this.helpAllSteps.Size = new System.Drawing.Size(68, 20);
+            this.helpAllSteps.Text = "Помощь";
+            // 
+            // allTabs
+            // 
+            this.allTabs.Controls.Add(this.tab1);
+            this.allTabs.Controls.Add(this.tabPage2);
+            this.allTabs.Location = new System.Drawing.Point(12, 27);
+            this.allTabs.Name = "allTabs";
+            this.allTabs.SelectedIndex = 0;
+            this.allTabs.Size = new System.Drawing.Size(824, 416);
+            this.allTabs.TabIndex = 2;
+            // 
+            // tab1
+            // 
+            this.tab1.Controls.Add(this.acceptFaultButton);
+            this.tab1.Controls.Add(this.label1);
+            this.tab1.Controls.Add(this.selectAccident);
+            this.tab1.Location = new System.Drawing.Point(4, 22);
+            this.tab1.Name = "tab1";
+            this.tab1.Padding = new System.Windows.Forms.Padding(3);
+            this.tab1.Size = new System.Drawing.Size(816, 390);
+            this.tab1.TabIndex = 0;
+            this.tab1.Text = "Загрузка данных";
+            this.tab1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(816, 390);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // selectAccident
+            // 
+            this.selectAccident.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.selectAccident.FormattingEnabled = true;
+            this.selectAccident.Location = new System.Drawing.Point(658, 107);
+            this.selectAccident.Name = "selectAccident";
+            this.selectAccident.Size = new System.Drawing.Size(150, 21);
+            this.selectAccident.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(655, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Выбранная авария";
+            // 
+            // acceptFaultButton
+            // 
+            this.acceptFaultButton.Enabled = false;
+            this.acceptFaultButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.acceptFaultButton.Location = new System.Drawing.Point(680, 255);
+            this.acceptFaultButton.Name = "acceptFaultButton";
+            this.acceptFaultButton.Size = new System.Drawing.Size(110, 48);
+            this.acceptFaultButton.TabIndex = 7;
+            this.acceptFaultButton.Text = "Подтвердить";
+            this.acceptFaultButton.UseVisualStyleBackColor = true;
+            // 
+            // MainFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 444);
+            this.Controls.Add(this.allTabs);
+            this.Controls.Add(this.menuStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainFrom";
+            this.Text = "DeepStatistic";
+            this.Resize += new System.EventHandler(this.MainFrom_Resize);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.allTabs.ResumeLayout(false);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem WorkFileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenFile;
+        private System.Windows.Forms.ToolStripMenuItem ExitApp;
+        private System.Windows.Forms.ToolStripMenuItem helpAllSteps;
+        private System.Windows.Forms.TabControl allTabs;
+        private System.Windows.Forms.TabPage tab1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button acceptFaultButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox selectAccident;
+    }
+}
+
---
+++ DeepParameters/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeepParameters {
+    public partial class MainFrom : Form {
+        public MainFrom() {
+            InitializeComponent();
+        }
+
+        private void MainFrom_Resize(object sender, EventArgs e) {
+            int newWidth = this.Width > 28 ? this.Width - 28 : 0;
+            int newHeight = this.Height > 67 ? this.Height - 67 : 0;
+            allTabs.Size = new Size(newWidth, newHeight);
+
+            // tab1
+
+        }
+    }
+}
